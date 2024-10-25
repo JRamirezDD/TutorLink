439ba1eea369e9a4005a04546d886a0d8eed52a1
Jorge Ramirez <jorge.ramirez1787@gmail.com>
2024-10-25 14:15:38 +0200

    Update AuthController.java

--- tutorlink/src/main/java/com/example/tutorlink/controller/AuthController.java
+++ tutorlink/src/main/java/com/example/tutorlink/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Auth Controller", description = "Authentication endpoints")
+@Tag(name = "Auth Controller", description = "Authentication Endpoints")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
