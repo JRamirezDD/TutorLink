d766f9240a22a755046aeafe767633b2fa37c813
Jorge Ramirez <jorge.ramirez1787@gmail.com>
2024-10-25 21:59:54 +0200

    Update AuthController.java

--- tutorlink/src/main/java/com/example/tutorlink/controller/AuthController.java
+++ tutorlink/src/main/java/com/example/tutorlink/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
-    @Operation(description = "Login User", summary = "User Login")
+    @Operation(description = "Login User", summary = "User login")
     @PostMapping(
             value = "/login",
             consumes = MediaType.APPLICATION_JSON_VALUE,
