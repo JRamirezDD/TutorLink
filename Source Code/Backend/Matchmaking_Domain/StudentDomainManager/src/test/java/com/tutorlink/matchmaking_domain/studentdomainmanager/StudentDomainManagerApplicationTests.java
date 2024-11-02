7c724d31416935b0f6f5b7c8851e01c08d7324f7
JRamirezDD <jorge.ramirez1787@gmail.com>
2024-11-02 14:51:43 +0100

    Project Skeleton

---
+++ Source Code/Backend/Matchmaking_Domain/StudentDomainManager/src/test/java/com/tutorlink/matchmaking_domain/studentdomainmanager/StudentDomainManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorlink.matchmaking_domain.studentdomainmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDomainManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
