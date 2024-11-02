ad98e4f96bb536167bf05976d6a03068274cd0cd
JRamirezDD <jorge.ramirez1787@gmail.com>
2024-11-02 14:51:43 +0100

    Project Skeleton

---
+++ Source Code/Backend/Matchmaking_Domain/CrossDomainServices/src/test/java/com/tutorlink/matchmaking_domain/crossdomaininteractions/CrossDomainInteractionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorlink.matchmaking_domain.crossdomaininteractions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossDomainInteractionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Source Code/Backend/Tutor_Domain/auth/src/main/java/com/tutorlink/student_domain/auth/AuthApplication.java
+++ Source Code/Backend/Tutor_Domain/auth/src/main/java/com/tutorlink/student_domain/auth/AuthApplication.java
@@ -1,4 +1,4 @@
-package com.tutorlink.student_domain.auth;
+package com.tutorlink.tutor_domain.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
