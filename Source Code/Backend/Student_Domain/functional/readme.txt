5dd6c45ad4d54a15c44a91fa3fffdd125347ab64
JRamirezDD <jorge.ramirez1787@gmail.com>
2024-11-14 21:57:00 +0100

    Student_Domain docker and docker-compose hosting setup

---
+++ Source Code/Backend/Student_Domain/functional/readme.txt
@@ -0,0 +1,3 @@
+
+How to run application?
+1. do 'docker-compose -p student-domain-functional up --build' whilst in the directory
--- Source Code/Backend/Student_Domain/functional/src/test/java/com/tutorlink/student_domain/functional/FunctionalApplicationTests.java
+++ Source Code/Backend/Student_Domain/functional/src/test/java/com/tutorlink/student_domain/functional/FunctionalApplicationTests.java
@@ -1,13 +1,13 @@
-package com.tutorlink.student_domain.functional;
+// package com.tutorlink.student_domain.functional;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FunctionalApplicationTests {
+// @SpringBootTest
+// class FunctionalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
