25dcbcd73bd9bfa71733fb08cb9e9e5dbf29f69e
Gio Kandelaki <giorgiikandelaki@gmail.com>
2024-11-28 17:48:22 +0100

    created APIs in matchmaking domain

--- SourceCode/Backend/Matchmaking_Domain/StudentDomainManager/src/main/java/com/tutorlink/matchmaking_domain/studentdomainmanager/controller/StudentProfileRetrievalController.java
+++ SourceCode/Backend/Matchmaking_Domain/StudentDomainManager/src/main/java/com/tutorlink/matchmaking_domain/studentdomainmanager/controller/StudentProfileRetrievalController.java
@@ -2,20 +2,21 @@ package com.tutorlink.matchmaking_domain.studentdomainmanager.controller;
 
 
 
+import com.tutorlink.matchmaking_domain.studentdomainmanager.controller.APIs.API_StudentProfileRetrieval;
 import com.tutorlink.matchmaking_domain.studentdomainmanager.model.dto.StudentProfileResp;
 import com.tutorlink.matchmaking_domain.studentdomainmanager.service.StudentProfileRetrievalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/students")
 @RequiredArgsConstructor
-public class StudentProfileRetrievalController {
+public class StudentProfileRetrievalController implements API_StudentProfileRetrieval {
 
     private final StudentProfileRetrievalService studentProfileRetrievalService;
 
-    @GetMapping("/{studentId}")
     public ResponseEntity<StudentProfileResp> getStudentProfile(@PathVariable Long studentId) {
         StudentProfileResp profile = studentProfileRetrievalService.getStudentProfile(studentId);
         return ResponseEntity.ok(profile);
--- SourceCode/Backend/Matchmaking_Domain/TutorDomainManager/src/main/java/com/tutorlink/matchmaking_domain/tutordomainmanager/servicemanagement/controller/ServiceManagementController.java
+++ SourceCode/Backend/Matchmaking_Domain/TutorDomainManager/src/main/java/com/tutorlink/matchmaking_domain/tutordomainmanager/servicemanagement/controller/ServiceManagementController.java
@@ -1,5 +1,6 @@
 package com.tutorlink.matchmaking_domain.tutordomainmanager.servicemanagement.controller;
 
+import com.tutorlink.matchmaking_domain.tutordomainmanager.servicemanagement.controller.APIs.API_ServiceManagement;
 import com.tutorlink.matchmaking_domain.tutordomainmanager.servicemanagement.model.dto.req.TutorServiceReq;
 import com.tutorlink.matchmaking_domain.tutordomainmanager.servicemanagement.model.dto.resp.TutorServiceResp;
 import com.tutorlink.matchmaking_domain.tutordomainmanager.servicemanagement.service.TutorServiceService;
@@ -12,29 +13,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/services")
 @RequiredArgsConstructor
-public class ServiceManagementController {
+public class ServiceManagementController implements API_ServiceManagement {
 
     private final TutorServiceService tutorServiceService;
 
-    @PostMapping
     public ResponseEntity<TutorServiceResp> createService(@RequestBody TutorServiceReq request) {
         TutorServiceResp response = tutorServiceService.createService(request);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/{serviceId}")
     public ResponseEntity<TutorServiceResp> getServiceById(@PathVariable Long serviceId) {
         TutorServiceResp response = tutorServiceService.getServiceById(serviceId);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping
     public ResponseEntity<List<TutorServiceResp>> getAllServices() {
         List<TutorServiceResp> response = tutorServiceService.getAllServices();
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping("/{serviceId}")
     public ResponseEntity<Void> deleteService(@PathVariable Long serviceId) {
         tutorServiceService.deleteService(serviceId);
         return ResponseEntity.noContent().build();
