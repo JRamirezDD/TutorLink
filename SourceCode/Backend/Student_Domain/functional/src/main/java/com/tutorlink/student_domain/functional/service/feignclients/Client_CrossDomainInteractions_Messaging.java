d1027b33a97981043737eba234851ee2bd63ac11
JRamirezDD <jorge.ramirez1787@gmail.com>
2024-12-02 14:00:54 +0100

    Modified gitignore

--- SourceCode/Backend/Student_Domain/functional/src/main/java/com/tutorlink/student_domain/functional/service/feignclients/Client_CrossDomainInteractions_Messaging.java
+++ SourceCode/Backend/Student_Domain/functional/src/main/java/com/tutorlink/student_domain/functional/service/feignclients/Client_CrossDomainInteractions_Messaging.java
@@ -1,9 +1,8 @@
 package com.tutorlink.student_domain.functional.service.feignclients;
 
-
 import com.tutorlink.matchmaking_domain.crossdomaininteractions.messaging.controller.APIs.API_Messaging;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "CrossDomainInteractions", url = "https://localhost:20011/messages")
+@FeignClient(value = "Client-CrossDomainInteractions-Messaging", url = "https://localhost:20011/messages")
 public interface Client_CrossDomainInteractions_Messaging extends API_Messaging {
 }
--- SourceCode/Backend/Student_Domain/functional/src/main/java/com/tutorlink/student_domain/functional/service/feignclients/Client_StudentDomainManager_Connection.java
+++ SourceCode/Backend/Student_Domain/functional/src/main/java/com/tutorlink/student_domain/functional/service/feignclients/Client_StudentDomainManager_Connection.java
@@ -3,9 +3,7 @@ package com.tutorlink.student_domain.functional.service.feignclients;
 import com.tutorlink.matchmaking_domain.crossdomaininteractions.connection.controller.APIs.API_ConnectionController;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "StudentDomainManager", url = "https://localhost:20011/connections")
-public interface Client_StudentDomainManager_Connection extends API_ConnectionController  {
+@FeignClient(value = "Client-StudentDomainManager-Connection", url = "https://localhost:20011/connections")
+public interface Client_StudentDomainManager_Connection extends API_ConnectionController {
 
 }
-
-
