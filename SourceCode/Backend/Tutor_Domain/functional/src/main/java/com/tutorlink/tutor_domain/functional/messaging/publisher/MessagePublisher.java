f5e48174b0645f9bcfb05da45401bc17a459fdf0
Gio Kandelaki <giorgiikandelaki@gmail.com>
2024-12-02 14:13:52 +0100

    fixed messagingPublisher in the tutor domain

--- SourceCode/Backend/Tutor_Domain/functional/src/main/java/com/tutorlink/tutor_domain/functional/messaging/publisher/MessagePublisher.java
+++ SourceCode/Backend/Tutor_Domain/functional/src/main/java/com/tutorlink/tutor_domain/functional/messaging/publisher/MessagePublisher.java
@@ -1,16 +1,36 @@
 package com.tutorlink.tutor_domain.functional.messaging.publisher;
 
+import com.tutorlink.matchmaking_domain.crossdomaininteractions.messaging.model.dto.req.SendMessageReq;
 import com.tutorlink.tutor_domain.functional.messaging.model.entity.Message;
+import com.tutorlink.tutor_domain.functional.messaging.service.feignclient.Client_CrossDomainInteractions_Messages;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class MessagePublisher {
 
+    private final Client_CrossDomainInteractions_Messages messagingClient;
+
     public void publishMessageEvent(Message message) {
-        //TODO: messaging
-        log.info("Publishing message event: {}", message);
+        try {
+            //map the local message entity to SendMessageReq DTO
+            SendMessageReq request = new SendMessageReq(
+                    message.getSenderId(),
+                    message.getReceiverId(),
+                    message.getContent(),
+                    message.getTimestamp()
+            );
+
+            //use feign client to send the message
+            messagingClient.sendMessage(request);
+
+            log.info("Successfully published message event: {}", message);
+        } catch (Exception e) {
+            log.error("Failed to publish message event for message: {}", message, e);
+        }
     }
 }
 
