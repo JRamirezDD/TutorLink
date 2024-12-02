9e27417954e8f8663f625f0321cb362236b87474
Gio Kandelaki <giorgiikandelaki@gmail.com>
2024-12-02 22:45:13 +0100

    test commit to see if this works.

--- SourceCode/Backend/Student_Domain/functional/src/main/java/com/tutorlink/student_domain/functional/service/MessagingService.java
+++ SourceCode/Backend/Student_Domain/functional/src/main/java/com/tutorlink/student_domain/functional/service/MessagingService.java
@@ -15,12 +15,12 @@ public class MessagingService {
     private final Client_CrossDomainInteractions_Messaging messagingClient;
 
     public MessageResp sendMessage(SendMessageReq req) {
-        // Delegate the sending of the message to the CrossDomainInteractions API
+        //delegate the sending of the message to the CrossDomainInteractions API
         return messagingClient.sendMessage(req).getBody();
     }
 
     public List<MessageResp> getMessagesBetweenUsers(Long userId, Long otherUserId) {
-        // Delegate the retrieval of messages to the CrossDomainInteractions API
+        //delegate the retrieval of messages to the CrossDomainInteractions API
         return (List<MessageResp>) messagingClient.getMessagesBetweenUsers(userId, otherUserId);
     }
 }
