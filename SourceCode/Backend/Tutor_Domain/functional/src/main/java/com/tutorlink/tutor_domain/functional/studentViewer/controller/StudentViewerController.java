52523a893d3ae28a306cebef23f0c2f2eebb5cde
Qviti <giorgiikandelaki@gmail.com>
2024-12-03 20:12:52 +0100

    added studentviewer client to tutor domain

--- SourceCode/Backend/Tutor_Domain/functional/src/main/java/com/tutorlink/tutor_domain/functional/studentViewer/controller/StudentViewerController.java
+++ SourceCode/Backend/Tutor_Domain/functional/src/main/java/com/tutorlink/tutor_domain/functional/studentViewer/controller/StudentViewerController.java
@@ -1,7 +1,6 @@
 package com.tutorlink.tutor_domain.functional.studentViewer.controller;
 
-
-import com.tutorlink.tutor_domain.functional.studentViewer.model.dto.resp.StudentProfileResp;
+import com.tutorlink.matchmaking_domain.studentdomainmanager.model.dto.resp.StudentProfileResp;
 import com.tutorlink.tutor_domain.functional.studentViewer.service.StudentViewerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,18 +15,14 @@ public class StudentViewerController {
 
     private final StudentViewerService studentViewerService;
 
-    //get specific student profile by ID
+    // Get specific student profile by ID
     @GetMapping("/{studentId}")
     public ResponseEntity<StudentProfileResp> getStudentProfile(@PathVariable Long studentId) {
         StudentProfileResp profile = studentViewerService.getStudentProfile(studentId);
         return ResponseEntity.ok(profile);
     }
 
-    //get all student profiles
-    @GetMapping
-    public ResponseEntity<List<StudentProfileResp>> getAllStudentProfiles() {
-        List<StudentProfileResp> profiles = studentViewerService.getAllStudentProfiles();
-        return ResponseEntity.ok(profiles);
-    }
 }
+
+
 
