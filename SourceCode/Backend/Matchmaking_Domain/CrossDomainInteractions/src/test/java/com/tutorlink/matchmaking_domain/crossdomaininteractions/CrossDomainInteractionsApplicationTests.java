913d75fd10652ca558ddad95fded53e4c5ffa353
Qviti <giorgiikandelaki@gmail.com>
2024-12-05 23:42:10 +0100

    fixed some tests

--- SourceCode/Backend/Matchmaking_Domain/CrossDomainInteractions/src/test/java/com/tutorlink/matchmaking_domain/crossdomaininteractions/CrossDomainInteractionsApplicationTests.java
+++ SourceCode/Backend/Matchmaking_Domain/CrossDomainInteractions/src/test/java/com/tutorlink/matchmaking_domain/crossdomaininteractions/CrossDomainInteractionsApplicationTests.java
@@ -1,14 +1,47 @@
 package com.tutorlink.matchmaking_domain.crossdomaininteractions;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import com.tutorlink.matchmaking_domain.crossdomaininteractions.rating.controller.RatingController;
+import com.tutorlink.matchmaking_domain.crossdomaininteractions.rating.model.dto.req.SubmitRatingReq;
+import com.tutorlink.matchmaking_domain.crossdomaininteractions.rating.model.dto.resp.RatingResp;
+import com.tutorlink.matchmaking_domain.crossdomaininteractions.rating.model.enums.StarRating;
+import com.tutorlink.matchmaking_domain.crossdomaininteractions.rating.service.RatingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class CrossDomainInteractionsApplicationTests {
-/*
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-	@Test
-	void contextLoads() {
-	}
-*/
+class RatingControllerTest {
 
+    @Mock
+    private RatingService ratingService;
+
+    @InjectMocks
+    private RatingController ratingController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testSubmitRating() {
+        SubmitRatingReq request = new SubmitRatingReq(123L, 456L, 5);
+        RatingResp mockResponse = RatingResp.builder()
+                .id(1L)
+                .tutorId(456L)
+                .ratingValue(StarRating.FIVE)
+                .build();
+
+        when(ratingService.submitRating(any(SubmitRatingReq.class))).thenReturn(mockResponse);
+
+        ResponseEntity<RatingResp> responseEntity = ratingController.submitRating(request);
+
+        assertEquals(mockResponse, responseEntity.getBody());
+    }
 }
--- SourceCode/Backend/Matchmaking_Domain/TutorDomainManager/src/test/java/com/tutorlink/matchmaking_domain/tutordomainmanager/TutorDomainManagerApplicationTests.java
+++ SourceCode/Backend/Matchmaking_Domain/TutorDomainManager/src/test/java/com/tutorlink/matchmaking_domain/tutordomainmanager/TutorDomainManagerApplicationTests.java
@@ -1,6 +1,5 @@
 package com.tutorlink.matchmaking_domain.tutordomainmanager;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
